a5dce05c8fa6d5a65cf1e7637c229c92eedab911
Eugene Taranov <eugene@taranov.me>
2020-06-17 00:01:43 +0300

    upd instance type t3.xlarge

--- aws/tf_eks/variables.tf
+++ aws/tf_eks/variables.tf
@@ -35,7 +35,7 @@ variable "node_group_settings" {
     desired_capacity = 1
     max_capacity     = 1
     min_capacity     = 1
-    instance_type    = "t3.small"
+    instance_type    = "t3.xlarge"
     disk_size        = 20
   }
 }
