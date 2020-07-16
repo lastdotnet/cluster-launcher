8aa2dbc1c7b16100edbd2f0e6964763bb839680c
Fandral <fandral.dev@gmail.com>
2020-07-17 09:22:11 +1000

    Update default instance

--- aws/variables.tf
+++ aws/variables.tf
@@ -41,7 +41,7 @@ variable "node_group_settings" {
     desired_capacity = 1
     max_capacity     = 4
     min_capacity     = 1
-    instance_type    = "t3.xlarge"
+    instance_type    = "m5.xlarge"
     disk_size        = 100
   }
 }
