37bfece3b5c5b03d8f6f151250733b9e95517d8f
Edouard Lavidalle <edouardlavidalle@gmail.com>
2020-08-19 12:36:16 +0000

    Correct Indentation

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
   kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.6.2"
+    source  = "gavinbunney/kubectl"
+    version = "1.6.2"
     }
   }
   required_version = ">= 0.12.0"
