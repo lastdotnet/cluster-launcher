d0a5483ca5c1bd882f98a795755534e8b4cb72f6
crstin <x-git@crstin.com>
2020-11-02 12:56:52 +0100

    Add comment to default instance type

--- linode/variables.tf
+++ linode/variables.tf
@@ -28,7 +28,8 @@ variable "pool_settings" {
   description = "Cluster pool settings"
   type        = map(string)
   default     = {
+    # Run this to get an overview of all instance types: `linode-cli linodes types`
     count            = 3
-    instance_type    = "g6-standard-8"
+    instance_type    = "g6-standard-8"  #  Linode 32GB with 8 CPU
   }
 }
