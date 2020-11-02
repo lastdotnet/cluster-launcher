b3d86dc1a27ef6cba3ee9b7e77840606cfe1f42e
crstin <x-git@crstin.com>
2020-11-02 13:07:09 +0100

    Unify provider and version files for linode and do

--- do/provider.tf
+++ do/provider.tf
@@ -1,7 +1,18 @@
-terraform {
-  required_version = ">= 0.12.0"
-}
-
 provider "digitalocean" {
   token = var.token
+}
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 1.22.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.4.0"
+    }
+  }
 }
--- linode/provider.tf
+++ linode/provider.tf
@@ -1,7 +1,18 @@
-terraform {
-  required_version = ">= 0.12.0"
-}
-
 provider "linode" {
   token = var.token
+}
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = ">= 1.13.3"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.4.0"
+    }
+  }
 }
