b334af272d098dbeb676079a86ddc6ec3d0976ba
crstin <x-git@crstin.com>
2021-06-06 16:58:54 +0200

    Update dependencies on AWS

--- aws/provider.tf
+++ aws/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.39"
+      version = ">= 3.45"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -12,7 +12,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0"
+      version = ">= 2.2"
     }
   }
 }
