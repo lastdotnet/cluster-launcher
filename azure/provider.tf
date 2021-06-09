5e7e6154f33a2d5cfce53a3634381777e34832fc
crstin <x-git@crstin.com>
2021-06-09 09:13:42 +0200

    Update dependencies on Azure

--- azure/provider.tf
+++ azure/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.56"
+      version = ">= 2.62"
     }
   }
 }
