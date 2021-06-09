ebf06609fc5d12c0dcab9abd7d8b67200459ad98
crstin <x-git@crstin.com>
2021-06-09 09:21:03 +0200

    Update dependencies on Digital Ocean

--- do/variables.tf
+++ do/variables.tf
@@ -11,7 +11,7 @@ variable "region" {
 variable "kubernetes_version" {
   description = "DigitalOcean Kubernetes cluster version"
   type        = string
-  default     = "1.19.6-do.0"
+  default     = "1.19.11-do.0"
 }
 
 variable "name" {
