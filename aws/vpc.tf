f0c038645c4e83cd4e4ed9a1f9074bd71acb25bb
crstin <x-git@crstin.com>
2021-10-03 10:05:08 +0200

    Update dependencies on AWS and lock VPC module

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.7.0"
 
   name           = var.cluster_name
   cidr           = var.vpc_cidr
